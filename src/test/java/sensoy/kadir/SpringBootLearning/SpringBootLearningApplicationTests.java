e2ab22ef5c70927b378da277496c64126f8cea9d
Kadir Şensoy <ksensoy@isbak.com.tr>
2022-06-29 10:02:55 +0300

    project started

---
+++ src/test/java/sensoy/kadir/SpringBootLearning/SpringBootLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package sensoy.kadir.SpringBootLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
